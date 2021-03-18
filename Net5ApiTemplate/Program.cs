1079b80e4349c7334ba28b467e79c58016c48383
harrygillingham99 <harrygillingham99@gmail.com>
2021-03-18 14:54:10 +0000

    fix: seperate info logging

--- Net5ApiTemplate/Program.cs
+++ Net5ApiTemplate/Program.cs
@@ -4,24 +4,40 @@ using System;
 using System.IO;
 using Serilog;
 using Serilog.Formatting.Json;
+using Serilog.Events;
+using System.Linq;
 
 namespace NetCore5ApiTemplate
 {
     public class Program
     {
+        private static readonly LogEventLevel[] errorLogs = new[] { LogEventLevel.Error, LogEventLevel.Fatal };
+        private static readonly LogEventLevel[] infoLogs = new[] { LogEventLevel.Information, LogEventLevel.Warning, LogEventLevel.Debug };
+
         public static void Main(string[] args)
         {
             Log.Logger = new LoggerConfiguration()
-                .MinimumLevel.Error()
-                .Enrich.FromLogContext()
-                .WriteTo.File(
-                    new JsonFormatter(renderMessage: true),
-                    Path.Combine(AppContext.BaseDirectory, "logs/Serilog.json"),
-                    shared: true,
-                    fileSizeLimitBytes: 20_971_520,
-                    rollOnFileSizeLimit: true,
-                    retainedFileCountLimit: 10)
-                .CreateLogger();
+                 .MinimumLevel.Information()
+                 .Enrich.FromLogContext()
+                 .WriteTo.Logger(lc => lc
+                     .Filter.ByIncludingOnly(ev => errorLogs.Contains(ev.Level))
+                     .WriteTo.File(
+                         new JsonFormatter(renderMessage: true),
+                         Path.Combine(AppContext.BaseDirectory, "logs//Serilog.json"),
+                         shared: true,
+                         fileSizeLimitBytes: 20_971_520, 
+                         rollOnFileSizeLimit: true,
+                         retainedFileCountLimit: 10))
+                 .WriteTo.Logger(lc => lc
+                     .Filter.ByIncludingOnly(ev => infoLogs.Contains(ev.Level))
+                     .WriteTo.File(
+                         new JsonFormatter(renderMessage: true),
+                         Path.Combine(AppContext.BaseDirectory, "logs//Info.json"),
+                         shared: true,
+                         fileSizeLimitBytes: 20_971_520,
+                         rollOnFileSizeLimit: true,
+                         retainedFileCountLimit: 10))
+                 .CreateLogger();
 
             try
             {
